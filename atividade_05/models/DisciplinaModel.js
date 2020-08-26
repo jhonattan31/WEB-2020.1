2eb67a9ca7d7a08d9b7d13bdac4f047ba8afefd6
jhonattan31 <39769250+jhonattan31@users.noreply.github.com>
2020-08-26 12:08:38 -0300

    atividade_05
    atividade_05

---
+++ atividade_05/models/DisciplinaModel.js
@@ -0,0 +1,12 @@
+class DisciplinaModel{
+
+    constructor(_id,nome,curso,quantidade){
+        this._id = _id
+        this.nome = nome
+        this.curso = curso
+        this.quantidade = quantidade
+    }
+
+}
+
+module.exports = DisciplinaModel
